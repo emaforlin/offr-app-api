aa0be26faddd1da0cb622afeee9650ac95102309
Ema Forlin <emaa.forlin@gmail.com>
2025-03-14 01:45:30 -0300

    implements role repository

--- infra/database/accounts.go
+++ infra/database/accounts.go
@@ -3,12 +3,36 @@ package database
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/emaforlin/offr-app-api/domain/entities"
 	"github.com/emaforlin/offr-app-api/domain/repositories"
 	"github.com/go-sql-driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
+
+// BindRoles implements repositories.AccountRepository.
+func (r *mysqlRepositoryImpl) BindRoles(ctx context.Context, accountID uint, roleIDs []uint) error {
+	var account entities.Account
+
+	if err := r.db.Preload("Roles").First(&account, accountID).Error; err != nil {
+		return errors.New("account not found")
+	}
+
+	var roles []entities.Role
+	if err := r.db.Where("id IN ?", roleIDs).Find(&roles).Error; err != nil {
+		return errors.New("failed to retrieve roles")
+	}
+
+	if err := r.db.Model(&account).Association("Roles").Replace(roles); err != nil {
+		fmt.Printf("error: %v", err)
+		return errors.New("role binding failed")
+	}
+
+	return nil
+
+}
 
 // Create implements AccountRepository.
 func (r *mysqlRepositoryImpl) Create(ctx context.Context, account *entities.Account) error {
@@ -29,7 +53,7 @@ func (r *mysqlRepositoryImpl) Create(ctx context.Context, account *entities.Acco
 func (r *mysqlRepositoryImpl) GetByEmail(ctx context.Context, email uint) (*entities.Account, error) {
 	var accountFound = &entities.Account{}
 
-	if err := r.db.First(accountFound, "email = ?", email).Error; err != nil {
+	if err := r.db.Preload(clause.Associations).First(accountFound, "email = ?", email).Error; err != nil {
 		return nil, errors.New("failed to find account")
 	}
 	return accountFound, nil
@@ -39,7 +63,7 @@ func (r *mysqlRepositoryImpl) GetByEmail(ctx context.Context, email uint) (*enti
 func (r *mysqlRepositoryImpl) GetByID(ctx context.Context, id uint) (*entities.Account, error) {
 	var accountFound = &entities.Account{}
 
-	if err := r.db.First(accountFound, id).Error; err != nil {
+	if err := r.db.Preload(clause.Associations).First(accountFound, id).Error; err != nil {
 		return nil, errors.New("failed to find account")
 	}
 	return accountFound, nil
